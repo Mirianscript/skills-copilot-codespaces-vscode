eb50020b6890a9e8f6a35d893c008e5294e36cfc
Mirian Ferreira <mirianferreirafr@gmail.com>
2025-04-24 01:22:28 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,9 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+function calculateNumbersWithDefault(var1 = 0, var2 = 0) {
+    return var1 + var2;
+}
+function calculateNumbersWithDefaultAndType(var1 = 0, var2 = 0) {
+    return var1 + var2;
+} 
